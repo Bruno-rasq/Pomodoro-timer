141ef6869972c9c59effeefae29c48a313a8604c
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-11-24 19:21:49 -0200

    display time funcionando

--- src/js/Main.js
+++ src/js/Main.js
@@ -5,12 +5,44 @@ const work_time = document.querySelector('#work_section_time')
 const break_time = document.querySelector('#break_section_time')
 
 const btn_time_controls = document.querySelectorAll('.btn-control')
+const play = document.querySelector('#btn_play')
+
+let aux_minutes;
+let aux_seconds = 0;
+
 
 
 const Debug = () => {
     console.log(work_time.value, break_time.value)
 
 }
+
+const setMin = () => {
+    if(work_time.value < 10){
+        minutes.innerHTML = `0${work_time.value}`
+    } else {
+        minutes.innerHTML = `${work_time.value}`
+    } 
+}
+
+const setSec = (time) => {
+
+    if(time < 10){
+        seconds.innerHTML = `0${time}`
+    } else {
+        seconds.innerHTML = `${time}`
+    }
+}
+
+const displayMin = (time) => {
+
+    if(time < 10){
+        minutes.innerHTML = `0${time}`
+    } else {
+        minutes.innerHTML = `${time}`
+    }
+}
+
 
 
 btn_time_controls.forEach((btn) => {
@@ -21,7 +53,32 @@ btn_time_controls.forEach((btn) => {
         if(btn.value === 'addtimebreak' && break_time.value < 10) break_time.value++
         if(btn.value === 'removetimebreak' && break_time.value > 0) break_time.value--
 
-        
-        Debug()
+        setMin()
+        // Debug()
     })
+})
+
+play.addEventListener('click', () => {
+
+    aux_minutes = work_time.value
+
+    let loop = setInterval(() => {
+
+        if(aux_seconds === 0){
+            aux_seconds = 60
+            aux_minutes--
+
+            displayMin(aux_minutes)
+        }
+
+        aux_seconds--
+        setSec(aux_seconds)
+
+        if(aux_minutes === 0 && aux_seconds === 0){
+            clearInterval(loop)
+            console.log('acabou o time')
+        } 
+
+    }, 1000)
+
 })
