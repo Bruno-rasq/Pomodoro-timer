f0d394567907b97abebef439f6bb4a1a55b5a972
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-11-28 00:39:31 -0200

    update html

--- src/js/timer.js
+++ src/js/timer.js
@@ -18,16 +18,16 @@ let aux1 = 25,
 
 let loop, 
     aux_minutes,
-    aux_seconds
+    aux_seconds = 0
 
 let passingTime = false,
     breaksession = false
 
-let countBreaksession = 0;
+let countBreaksession = 0
 
 
 
-const displayTime = (element, time) => {
+const displayTime = ( element, time ) => {
 
     if(time < 10){
         element.innerHTML = `0${time}`
@@ -36,15 +36,15 @@ const displayTime = (element, time) => {
     }
 }
 
-const sessionDuration = ( element, value) => {
+const sessionDuration = ( element, value ) => {
     element.innerHTML = `${value} min`
 }
 
-const displayText = (text) => {
+const displayText = ( text ) => {
     title.innerHTML = text
 }
 
-const StartTime = ( min = 25, sec = 0) => {
+const StartTime = ( min = 25, sec = 0 ) => {
 
     passingTime = true
     aux_minutes = min
@@ -73,35 +73,35 @@ const EndTime = () => {
     if(aux_minutes === 0 && aux_seconds === 0){
 
         clearInterval(loop)
-        passingTime = false
+        audio.play()
 
+        passingTime = false
         displayText('End session')
-        console.log('acabou o time')
         play.removeAttribute('disabled')
 
-        audio.play()
-        btn_time_controls.forEach((btn) => btn.removeAttribute("disabled"))
 
         setTimeout(()=> {
 
             breaksessionTime()
 
         }, 3000)
-    
     }
-
 }
 
 const breaksessionTime = () => {
 
-    displayText('break session')
-    btn_time_controls.forEach((btn) => btn.removeAttribute("disabled"))
+    displayText('Break session')
+    passingTime = true
+
     if(countBreaksession === 4 && breaksession === false){
 
+        displayText('Break session bônus')
         StartTime(10)
         countBreaksession = 0
+        breaksession = true
 
     } else if(breaksession === false){
+
         StartTime(aux2)
         breaksession = true
         countBreaksession++
@@ -118,7 +118,7 @@ const playTime = () => {
         passingTime = true
         btn_time_controls.forEach((btn) => btn.setAttribute("disabled", true))
 
-        displayText('Go! - work session')
+        displayText('Work session')
         StartTime(aux1, aux_seconds)
 
         play.setAttribute('disabled', true)
@@ -128,20 +128,18 @@ const playTime = () => {
     }
 }
 
-// Bug na função pauseTime: timer se comportando estranho somente quandfo o pause é acionado.
 const pauseTime = () => {
 
     if(passingTime === true){
         
         clearInterval(loop)
+        displayText('paused')
         passingTime = false
 
-        displayText('paused')
         play.removeAttribute('disabled')
         play.textContent = 'continue'
-        pause.classList.add('active')
         play.classList.remove('active')
-
+        pause.classList.add('active')
     }
 }
 
@@ -154,6 +152,7 @@ const resetTime = () => {
     aux1 = 25
     aux2 = 5
     aux_seconds = 0
+    aux_minutes = 0
 
     displayText('Are you ready?')
     sessionDuration( work_time, aux1)
@@ -172,9 +171,9 @@ const resetTime = () => {
 
 
 window.onload = () => {
-    work_time.innerHTML = `${aux1} min`
-    break_time.innerHTML = `${aux2} min`
 
+    sessionDuration(work_time, aux1)
+    sessionDuration(break_time, aux2)
     displayTime(minutes, aux1)
 }
 
